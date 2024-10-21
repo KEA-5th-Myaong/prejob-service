a384adf4c979dd3d607b3adba335cc718dc0d264
Sihun23 <gcsw16@Gachonui-MacBookPro.local>
2024-10-22 01:08:24 +0900

    :recycle:refactor:API description 어노테이션 추가

--- src/main/java/myaong/popolog/prejobsservice/controller/PrejobController.java
+++ src/main/java/myaong/popolog/prejobsservice/controller/PrejobController.java
@@ -1,5 +1,6 @@
 package myaong.popolog.prejobsservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import myaong.popolog.prejobsservice.common.exception.ApiResponse;
@@ -18,21 +19,21 @@ public class PrejobController {
 
     private final PrejobService prejobService;
 
-    // 관심 직군 목록 조회 (카테고리별)
+    @Operation(summary = "API 명세서 v0.3 line 27", description = "관심직군 목록 조회")
     @GetMapping("/jobs")
     public ResponseEntity<ApiResponse<List<PrejobResponse>>> getAvailableJobs() {
         List<PrejobResponse> response = prejobService.getAvailableJobs();
         return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 
-    // 회원의 관심 직군 목록 조회
+    @Operation(summary = "API 명세서 v0.3 line 28", description = "회원의 관심직군 목록 조회")
     @GetMapping
     public ResponseEntity<ApiResponse<List<PrejobResponse.JobDetail>>> getMemberPrejobs() {
         List<PrejobResponse.JobDetail> response = prejobService.getPrejobsForMember(5L);
         return ResponseEntity.ok(ApiResponse.onSuccess(response));
     }
 
-    // 관심 직군 등록/수정
+    @Operation(summary = "API 명세서 v0.3 line 29", description = "관심직군 등록/수정")
     @PostMapping
     public ResponseEntity<ApiResponse<Object>> savePrejobs(@Valid @RequestBody PrejobRequest prejobRequest) {
         prejobService.savePrejobs(5L, prejobRequest);  // memberId 5 고정
